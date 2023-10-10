e1f4be7db4befb2fa9f85e514796c786351afe3b
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-10-10 11:38:21 +0200

    CALCULADORA DE ÁREA Y PERÍMETRO DE UN RECTÁNGULO

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// JAIME FERNÁNDEZ CALVO
+// 
+
+function calcularAreaPerimetro(){
+    let longitud = document.getElementById("longitud").value;
+    let ancho = document.getElementById("ancho").value;
+
+    let area = 0;
+    let perimetro = 0;
+
+    if(longitud > 0 && ancho > 0){
+        area = longitud * ancho;
+        perimetro = (longitud * longitud) + (ancho * ancho);
+    }
+
+    document.getElementById("area").innerHTML = "Ärea del rectangulo: " + area;
+    document.getElementById("perim").innerHTML = "Longitud del rectángulo: " + perimetro;
+}
